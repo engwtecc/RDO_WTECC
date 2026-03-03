4742caa3aebccfb402ffcc29a7cb1042448d8616
engwtecc <engenharia@wtecc.com.br>
2026-03-03 10:44:49 -0300

    Ensure DATABASE_URL is defined in the environment
    Added a check for DATABASE_URL to raise an exception if not defined.

--- app/database.py
+++ app/database.py
@@ -7,7 +7,11 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL não definida no ambiente.")
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
+
